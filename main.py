70ddd3b7991884ff479a5053302fb12088181c05
Allison <greendev32@gmail.com>
2022-08-07 15:31:09 -0400

    Create YOLO style ROIs and classifications on full images

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ if __name__ == '__main__':
     #             'C:/Users/green/Pictures/DATA/test/',
     #             200,
     #             200)
+    # sc.classifyROI('C:/Users/green/PycharmProjects/stemClassifier/my_model',
+    #                'C:/Users/green/Pictures/DATA/stem_dataset/',
+    #                True)
 
     # Simplified dataset
     # sc.loadDataset('C:/Users/green/Pictures/DATA/simplified_roi_dataset/shuffled/',
@@ -23,10 +26,13 @@ if __name__ == '__main__':
     #                400)
     #sc.createModel()
     #sc.trainModel('quadrant')
-    sc.classify('C:/Users/green/PycharmProjects/stemClassifier/batch_normalization',
-                'C:/Users/green/Pictures/DATA/simplified_roi_dataset/test_set/',
-                400,
-                400)
+    # sc.classify('C:/Users/green/PycharmProjects/stemClassifier/batch_normalization',
+    #             'C:/Users/green/Pictures/DATA/simplified_roi_dataset/test_set/',
+    #             400,
+    #             400)
+    # sc.classifyROI('C:/Users/green/PycharmProjects/stemClassifier/batch_normalization',
+    #                'C:/Users/green/Pictures/DATA/simplified_dataset2/',
+    #                False)
 
     # ResNet50 Test
     # sc.loadDataset('C:/Users/green/Pictures/DATA/simplified_roi_dataset/shuffled/',
--- venv/stemClassifier/stemClassifier.py
+++ venv/stemClassifier/stemClassifier.py
@@ -6,15 +6,17 @@
 # This script includes a GUI that displays the results of the
 # model running on new images from the test dataset.
 
+import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 import tensorflow as tf
 
 from future.moves import tkinter
+from io import BytesIO
 from os import listdir
 from os.path import isfile, join
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw, ImageFont
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
@@ -25,6 +27,8 @@ class StemClassifier:
         self.img_height = 0
         self.batch_size = 32
         self.epochs = 10
+        self.roi = False # ROI mode
+        self.originalDataset = True
 
         class_names = ['0\u00B0 to 45\u00B0',
                        '135\u00B0 to 180\u00B0',
@@ -163,7 +167,10 @@ class StemClassifier:
         if self.imgIdx > (len(self.onlyfiles) - 1):
             self.imgIdx = 0
 
-        self.updateImage()
+        if self.roi:
+            self.updateImageROI()
+        else:
+            self.updateImage()
 
     def previousButton(self):
         self.imgIdx -= 1
@@ -171,7 +178,10 @@ class StemClassifier:
         if self.imgIdx < 0:
             self.imgIdx = (len(self.onlyfiles) - 1)
 
-        self.updateImage()
+        if self.roi:
+            self.updateImageROI()
+        else:
+            self.updateImage()
 
     def updateImage(self):
         img = tf.keras.utils.load_img(self.imgDir + self.onlyfiles[self.imgIdx],
@@ -204,6 +214,132 @@ class StemClassifier:
         imgTk.grid(column=1, row=0, columnspan=1, rowspan=1, padx=20, pady=20)
         self.imageLabel.grid(column=1, row=1, padx=20, pady=20)
 
+    def updateImageROI(self):
+        fileName = self.imgDir + self.onlyfiles[self.imgIdx]
+        self.img = cv.imread(fileName)  # store original image
+        imgDraw = self.img.copy()  # cloned image for drawing purposes
+
+        dilation_size = 5
+        erosion_size = 3
+
+        erosion_element = cv.getStructuringElement(cv.MORPH_RECT, (erosion_size, erosion_size))
+        dilation_element = cv.getStructuringElement(cv.MORPH_RECT, (dilation_size, dilation_size))
+
+        eroded = cv.erode(imgDraw, erosion_element, iterations=1)
+        dilated = cv.dilate(eroded, dilation_element, iterations=1)
+
+        b, g, r = cv.split(dilated)
+
+        # amplify red color
+        r = cv.multiply(r, 0.8)
+
+        # reduce blue and green colors
+        redProcess = cv.subtract(r, b)
+        redProcess = cv.subtract(redProcess, g)
+
+        # threshold using red color
+        ret, thresh = cv.threshold(redProcess, 20, 255,
+                                   cv.THRESH_BINARY)
+
+        # find red contours
+        contours, hierarchies = cv.findContours(
+            thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+        contourFound = False
+        if self.originalDataset:
+            # draw valid contours
+            for contour in contours:
+                moment = cv.moments(contour)
+                if moment['m00'] != 0:
+                    Array_Size = len(contour)
+                    # center of the contour
+                    cx = int(moment['m10'] / moment['m00'])
+                    cy = int(moment['m01'] / moment['m00'])
+                    if (cv.contourArea(contour) > 100) and (cy-100 >= 0) and (cy+100 <= 1280) and (cx-100 >= 0) and (cx+100 <= 720):
+                        #cv.drawContours(imgDraw, [contour], -1, (0, 255, 0), 2)
+                        roi = self.img[cy - 100:cy + 100, cx - 100:cx + 100]
+                        cv.imwrite("temp.png", roi)
+
+                        img = tf.keras.utils.load_img("temp.png",
+                                                      target_size=(200, 200))
+
+                        img_array = tf.keras.utils.img_to_array(img)
+                        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+                        predictions = self.model.predict(img_array)
+                        score = tf.nn.softmax(predictions[0])
+                        labelText = str(self.class_names[np.argmax(score)])
+
+                        # draw a rectangle centering on each contour
+                        cv.rectangle(self.img, (cx-100, cy-100), (cx+100, cy+100), (255, 0, 0), 2)
+                        # draw rectangle for label background
+                        cv.rectangle(self.img, (cx - 101, cy - 130), (cx + 70, cy - 100), (255, 0, 0), -1)
+                        contourFound = True
+
+                        # put label on each ROI
+                        pil_image = Image.fromarray(self.img)
+                        draw = ImageDraw.Draw(pil_image)
+                        font_file = open("C:\Windows\Fonts\\trebucbd.ttf", "rb")
+                        bytes_font = BytesIO(font_file.read())
+                        draw.text((cx-90, cy-128),
+                                  labelText,
+                                  font=ImageFont.truetype(bytes_font, 24),
+                                  fill="#00FF00")
+                        self.img = np.asarray(pil_image)
+        else:
+            areas = [cv.contourArea(c) for c in contours]
+            max_index = np.argmax(areas)
+            contour = contours[max_index]
+            moment = cv.moments(contour)
+            if moment['m00'] != 0:
+                # center of the contour
+                cx = int(moment['m10'] / moment['m00'])
+                cy = int(moment['m01'] / moment['m00'])
+
+                roi = self.img[cy - 200:cy + 200, cx - 200:cx + 200]
+                cv.imwrite("temp.png", roi)
+
+                img = tf.keras.utils.load_img("temp.png",
+                                              target_size=(400, 400))
+
+                img_array = tf.keras.utils.img_to_array(img)
+                img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+                predictions = self.model.predict(img_array)
+                score = tf.nn.softmax(predictions[0])
+                labelText = str(self.class_names[np.argmax(score)])
+
+                cv.rectangle(self.img, (cx - 200, cy - 200), (cx + 200, cy + 200), (255, 0, 0), 2)
+                cv.rectangle(self.img, (cx - 201, cy - 230), (cx - 30, cy - 200), (255, 0, 0), -1)
+                contourFound = True
+
+                # put label on each ROI
+                pil_image = Image.fromarray(self.img)
+                draw = ImageDraw.Draw(pil_image)
+                font_file = open("C:\Windows\Fonts\\trebucbd.ttf", "rb")
+                bytes_font = BytesIO(font_file.read())
+                draw.text((cx - 190, cy - 228),
+                          labelText,
+                          font=ImageFont.truetype(bytes_font, 24),
+                          fill="#00FF00")
+                self.img = np.asarray(pil_image)
+
+        if contourFound:
+            self.img = cv.cvtColor(self.img, cv.COLOR_BGR2RGB)
+            if self.originalDataset == False:
+                self.img = self.img[200:1480, 200:920]
+
+            self.img = Image.fromarray(self.img)
+            self.img = self.img.resize((360, 640))  # display all images as 360x640px
+            self.img = ImageTk.PhotoImage(self.img)
+            imgTk = tkinter.Label(self.content, image=self.img)
+            imgTk.grid(column=1, row=0, columnspan=1, rowspan=1, padx=20, pady=20)
+        else:
+            # remove and skip images with no ROIs
+            del self.onlyfiles[self.imgIdx]
+            self.imgIdx += 1
+            self.nextButton()
+
     def classify(self, modelDirName, imgDir, imgWidth, imgHeight):
         # get all image files and model
         self.imgDir = imgDir
@@ -218,6 +354,37 @@ class StemClassifier:
         # create a GUI
         self.root = tkinter.Tk()
         self.root.geometry("615x500")
+        self.content = tkinter.Frame(self.root)
+        self.content.grid(column=0, row=0)
+        self.imageLabel = tkinter.Label(self.content, text="")
+
+        # run model on first image
+        self.nextButton()
+
+        # define buttons
+        button1 = tkinter.Button(self.content, text='Next', command=self.nextButton)
+        button2 = tkinter.Button(self.content, text='Previous', command=self.previousButton)
+
+        # put buttons in the display window
+        button2.grid(column=0, row=0, padx=20, pady=20)
+        button1.grid(column=2, row=0, padx=20, pady=20)
+
+        self.root.title("Stem Classifier")
+        self.root.mainloop()  # Start the GUI
+
+    def classifyROI(self, modelDirName, imgDir, originalDataset):
+        # get all image files and model
+        self.imgDir = imgDir
+        self.originalDataset = originalDataset
+        self.onlyfiles = [f for f in listdir(imgDir) if isfile(join(imgDir, f))]
+        random.shuffle(self.onlyfiles)
+        self.model = tf.keras.models.load_model(modelDirName)
+        self.imgIdx = -1
+        self.roi = True # enable ROI mode
+
+        # create a GUI
+        self.root = tkinter.Tk()
+        self.root.geometry("575x700")
         self.content = tkinter.Frame(self.root)
         self.content.grid(column=0, row=0)
         self.imageLabel = tkinter.Label(self.content, text="")
