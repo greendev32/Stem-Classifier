670614bfd8a9048b7b310079885688c441aa1a2d
Allison <greendev32@gmail.com>
2022-08-07 10:50:26 -0400

    Update test driver

--- main.py
+++ main.py
@@ -7,19 +7,34 @@ if __name__ == '__main__':
     sc = stemClassifier.StemClassifier()
 
     # Original dataset
-    #sc.loadDataset('C:/Users/green/Pictures/DATA/split_dataset/', 200, 200)
+    # sc.loadDataset('C:/Users/green/Pictures/DATA/split_dataset/',
+    #                200,
+    #                200)
     #sc.createModel()
     #sc.trainModel('my_model')
-    #sc.classify('C:/Users/green/PycharmProjects/stemClassifier/my_model', 'C:/Users/green/Pictures/DATA/test/')
+    # sc.classify('C:/Users/green/PycharmProjects/stemClassifier/my_model',
+    #             'C:/Users/green/Pictures/DATA/test/',
+    #             200,
+    #             200)
 
     # Simplified dataset
-    # sc.loadDataset('C:/Users/green/Pictures/DATA/simplified_roi_dataset/shuffled/', 400, 400)
-    # sc.createModel()
-    # sc.trainModel('simplified_model')
-    #sc.classify('C:/Users/green/PycharmProjects/stemClassifier/simplified_model', 'C:/Users/green/Pictures/DATA/simplified_roi_dataset/test_set/')
+    # sc.loadDataset('C:/Users/green/Pictures/DATA/simplified_roi_dataset/shuffled/',
+    #                400,
+    #                400)
+    #sc.createModel()
+    #sc.trainModel('quadrant')
+    sc.classify('C:/Users/green/PycharmProjects/stemClassifier/batch_normalization',
+                'C:/Users/green/Pictures/DATA/simplified_roi_dataset/test_set/',
+                400,
+                400)
 
     # ResNet50 Test
-    sc.loadDataset('C:/Users/green/Pictures/DATA/simplified_roi_dataset/shuffled/', 400, 400)
-    sc.createResNet50Model()
-    sc.trainModel('resnet50_model')
-    #sc.classify('C:/Users/green/PycharmProjects/stemClassifier/resnet50_model', 'C:/Users/green/Pictures/DATA/simplified_roi_dataset/test_set/')
+    # sc.loadDataset('C:/Users/green/Pictures/DATA/simplified_roi_dataset/shuffled/',
+    #                400,
+    #                400)
+    #sc.createResNet50Model()
+    #sc.trainModel('resnet50_model')
+    # sc.classify('C:/Users/green/PycharmProjects/stemClassifier/resnet50_model',
+    #             'C:/Users/green/Pictures/DATA/simplified_roi_dataset/test_set/',
+    #             400,
+    #             400)
